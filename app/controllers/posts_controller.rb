0a14d4c827dddab22a8653a91643280bd6005607
N3ggro <n3ggro@gmail.com>
2014-04-23 17:16:56 -0300

    está mas bonita la cosa ahora xd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,17 @@
 class PostsController < ApplicationController
 	
 	def new
+		@post = Post.new
 	end
 
 	def create
-  		render text: params[:post].inspect
-	end
+		@post = Post.new(params[:post].permit(:title, :text))
 
-	def create
-		@post = Post.new(post_params)
-
-		@post.save
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
@@ -21,6 +21,28 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def edit
+  		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
 	 
 	private def post_params
 	    params.require(:post).permit(:title, :text)
